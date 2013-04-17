c8ecc3e74eb1c3d962aa43e1beed1f930915b447
Nick Pelone <npelone@unca.edu>
2013-04-17 14:45:28 +0000

    Modularization of tests, define callbacks for functions

--- server.js
+++ server.js
@@ -2,6 +2,19 @@
  copyright/left 2013 nickpelone
 **/
 
+/** Basic ideas of functions and responses:
+    a file browsing or navigation operation should always return the current dir in a callback
+    ex. moveUpDir( function (currentDirectory) {
+        console.log(currentDirectory);
+        });
+    
+    This should be done by defining functions with callbacks:
+    ex.
+    function moveUpDir(callback) {
+      //the next line is a function prototype
+      callback(local, function, variables, here);
+    }
+**/
 
 
 var http = require('http'),
@@ -10,6 +23,7 @@ var http = require('http'),
     express = require('express'),
     app = express(),
     currentDir,
+    startDir,
     sys = require('sys'),
     exec = require('child_process').exec,
     cdUp,
@@ -17,32 +31,53 @@ var http = require('http'),
     oldDir;
     
     
-function moveUpDir() {
+function moveUpDir(callback) {
   cdUp = exec("cd .. && pwd", function(error, stdout, stderr) {
     if(error) throw error;
     if(stderr){
       console.log(stderr);
     }
-    //console.log(stdout);
     currentDir = stdout;
+    callback(currentDir);
   });  
 }
-function getCurrentDir() {
-    pWd = exec("pwd", function(err, stdout, stderr) {
-    if(err) throw err;
-    if(stderr) throw stderr;
-    var returnable ="";
-    returnable = returnable + stdout;
-    console.log("trying to return something " + returnable);
-    return stdout;
+
+function debugTests(){
+  getCurrentDir(function (response){ 
+    console.log("1. Current working directory: " + response);
+      moveUpDir(function (response) {
+        console.log("2. We tried to move up: " + response);
+        getCurrentDir(function (response) {
+          console.log("3. This should be the same as before: " + response);
+        });
+    });
   });
 }
+function getCurrentDir(callback) {
+  pWd = exec("pwd", function(error, stdout, stderr) {
+    if (error) throw error;
+    if (stderr) console.log("stderr!! " + stderr);
+    currentDir = stdout;
+    currentDir = currentDir.substring(0,currentDir.length - 1);
+    callback(currentDir);
+  });
+}
+
+function listCurrentDir(){
+  getCurrentDir(function (currentDirectory){
+    fs.readdir(currentDirectory, function(err, res){
+      for(var i = 0; i<res.length;i++){
+        console.log(res[i]);
+      }
+    });
+  });
+}
+  
 // This is our basic configuration                                                                                                                     
 app.configure(function () {
     // Define our static file directory, it will be 'public'                                                                                           
     app.use(express.static(path.join(__dirname, 'public')));
 });
-
 app.get("/remote", function(req,res) {
     res.send("A remote will be here, one day.");
 });
@@ -56,18 +91,10 @@ app.get("/currentDir.json", function(req,res) {
 //create our express http server here
 http.createServer(app).listen(3000,function() {
     console.log("Express server is up on port 3000");
-    //print out a directory(the current one)
-    //get current directory
-    currentDir = "/home/vagrant/app";
-    fs.readdir(currentDir, function(err,res) {
-      if(err) throw err;
-      for(var i =0; i<res.length;i++) {
-        console.log(res[i]);
-      }
-      console.log("trying to move up from app folder");
-      moveUpDir();
-      console.log("new value of currentDir: " + currentDir);
-      console.log("old directory we were in: "+ oldDir);
-      console.log("get current directory result: " + getCurrentDir());
+    //
+    getCurrentDir( function (response){ 
+      console.log("first working directory:" + response);
+      listCurrentDir();
     });
+    //
   });
